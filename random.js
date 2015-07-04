9f51d2a2e9c77c28f5dfb708aa6f5730f60da755
ungong <castell_r@wanadoo.es>
2015-07-04 22:34:53 +0200

    random versión 2

--- random.js
+++ random.js
@@ -1,9 +1,10 @@
 // Math.ramdom() devuelve número aleatorio entre 0 y 1
 var numero = Math.random();
 
+var str =' MAYOR que 0,5':
+
 if (numero <= 0.5){
-  console.console.log('\n' + numero + ' Menor que 0,5 \n');
+  str=' MENOR que 0,5');
 }
-else{
-  console.log('\n' + numero + ' Mayor que 0,5 \n');
-}
+
+console.log('\n' + numero + str + '\n');
